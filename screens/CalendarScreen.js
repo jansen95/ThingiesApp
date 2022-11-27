e75646f56148f2ac5eb84b5712993098fbcab8c0
Maximilian Möllmann <maximilian.moellmann@studmail.w-hs.de>
2022-11-27 10:47:30 +0100

    Calendar Hello World

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,9 +1,25 @@
 import {Text, View} from "react-native";
+import TodoListsTextScreen from "./TodoListsTextScreen";
+import {CalendarList} from "react-native-calendars";
 
 export default function CalendarScreen() {
     return(
         <View>
-            <Text>Calendar Screen</Text>
+            <CalendarList
+                onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+                pastScrollRange={50}
+                futureScrollRange={50}
+                scrollEnabled={true}
+                showScrollIndicator={true}
+
+                 maxDate={new Date()}
+
+                onDayPress={day => {console.log('selected day', day);}}
+
+            />
+            <TodoListsTextScreen/>
+
+
         </View>
     )
 }
