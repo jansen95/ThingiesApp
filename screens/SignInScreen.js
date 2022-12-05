69cae7709e96edd4b38bbd243157e7ab063cd2f0
Alexander Jansen <alexander.jansen@studmail.w-hs.de>
2022-12-05 23:03:07 +0100

    styling for SignInScreen.js & SignUpScreen.js

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,32 +1,62 @@
-import {Button, TextInput, View} from "react-native";
+import {Button, View, StyleSheet} from "react-native";
 import React from "react";
 import {AuthContext} from "../state/AuthContext";
+import {useThemeType} from "../state/ThemeProvider";
+import {THEME_COLORS} from "../state/ThemeColors";
+import {TextInput} from "react-native-paper";
 
 export default function SignInScreen({navigation}) {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
 
     const { signIn } = React.useContext(AuthContext);
+    const darkTheme = useThemeType();
 
     return (
-        <View>
+        <View style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+              backgroundColor={darkTheme ?
+                    THEME_COLORS.DARK_THEME.SURFACE + THEME_COLORS.DARK_THEME.ON_SURFACE_OPACITY.HIGH_EMPHASIS :
+                    THEME_COLORS.LIGHT_THEME.SURFACE + THEME_COLORS.LIGHT_THEME.ON_SURFACE_OPACITY.MEDIUM_EMPHASIS}
+        >
             <TextInput
-                placeholder="Username"
-                value={username}
+                style={styles.textInput}
+                label="Username"
                 onChangeText={setUsername}
             />
             <TextInput
-                placeholder="Password"
-                value={password}
+                style={styles.textInput}
+                label="Password"
                 onChangeText={setPassword}
                 secureTextEntry
             />
-            <Button title="Sign in" onPress={() => signIn({ username, password })} />
-            <Button
-                title="Go to Sign Up"
-                onPress={() => navigation.navigate('SignUp')}
-            />
-
+            <View style={styles.buttons}>
+                <Button
+                    title="Sign in"
+                    onPress={() => signIn({ username, password })}
+                    color={darkTheme ? THEME_COLORS.DARK_THEME.PRIMARY : THEME_COLORS.LIGHT_THEME.PRIMARY}
+                />
+                <Button
+                    title="Go to Sign Up"
+                    onPress={() => navigation.navigate('SignUp')}
+                    color={darkTheme ? THEME_COLORS.DARK_THEME.PRIMARY : THEME_COLORS.LIGHT_THEME.PRIMARY}
+                />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+    textInput: {
+        width: 300
+    },
+    buttons: {
+        paddingTop: 8,
+    }
+
+});
+
