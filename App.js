ea69806355e6750af42321176743e6dc0536bd81
Hendrik Nieuwmeyer <Hendrik.Nieuwmeyer@studmail.w-hs.de>
2023-02-01 17:36:59 +0100

    -git_ignore app.json eas.json fixxed for eas

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React, {} from "react";
+import React, {useEffect} from "react";
 import ThemeProvider from "./state/ThemeProvider";
 import TodoListProvider from "./state/TodoListProvider";
 import {NavigationContainer} from "@react-navigation/native";
 import LoginStackNavigator from "./authentication/LoginStackNavigator";
 import CustomStatusBar from "./components/CustomStatusBar";
-import { useColorScheme } from 'react-native';
+import { useColorScheme, Platform } from 'react-native';
 import * as NavigationBar from "expo-navigation-bar";
 import {THEME_COLORS} from "./state/ThemeColors";
 import * as Device from 'expo-device';
 import {GestureHandlerRootView} from "react-native-gesture-handler";
-
+import * as Notifications from 'expo-notifications';
 
 
 export default function App() {
@@ -22,6 +22,42 @@ export default function App() {
             THEME_COLORS.LIGHT_THEME.BACKGROUND
         ).then()
     }
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+
+        if (Platform.OS === 'android') {
+            await Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        if (Device.isDevice) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+            console.log(token);
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
+
+        return token;
+    }
+
+    useEffect(() =>{
+        (()=> registerForPushNotificationsAsync())();
+    },[]);//*/
 
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
