9d96e253775b45d70e00d6e0d09fa540053a9473
Alexander Jansen <alexander.jansen@studmail.w-hs.de>
2023-02-02 01:23:05 +0100

    DrawerContent.js: added Input field for adding list (just visually)

--- navigation/DrawerContent.js
+++ navigation/DrawerContent.js
@@ -6,11 +6,15 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {API_ADDRESS} from '@env';
 import {useToken} from "../state/TokenContext";
+import {ScrollView} from "react-native";
+import {useThemeType} from "../state/ThemeProvider";
+import {TextInput} from "react-native-paper";
 
 export default function DrawerContent(props) {
     const [todosLists, dispatchTodoLists, activeList, setActiveList] = useTodoLists();
     const [databaseLists, setDatabaseLists] = useState([{"title": "List"}]);
     const token = useToken();
+    const darkTheme = useThemeType();
 
 
     useEffect(() => {
@@ -29,31 +33,42 @@ export default function DrawerContent(props) {
     }, []);
 
     return(
-        <DrawerContentScrollView {...props}>
-            <DrawerItem key={"All List"}
-                               label={"All List"}
-                               activeBackgroundColor={THEME_COLORS.LIGHT_THEME.PRIMARY}
-                               activeTintColor={THEME_COLORS.LIGHT_THEME.ON_PRIMARY}
-                               focused={activeList === 0}
-                               onPress={() => {
-                                   setActiveList(0);
-                                   props.navigation.navigate("All List");
-                               }}
-                               active={true}
-            />
-            {databaseLists.map((list, listIndex) => {
-                return <DrawerItem key={list.title}
-                                   label={list.title}
-                                   activeBackgroundColor={THEME_COLORS.LIGHT_THEME.PRIMARY}
-                                   activeTintColor={THEME_COLORS.LIGHT_THEME.ON_PRIMARY}
-                                   focused={list.id === activeList}
-                                   onPress={() => {
-                                       setActiveList(list.id);
-                                       props.navigation.navigate(list.title);
-                                   }}
-                                   active={true}
+        <DrawerContentScrollView {...props} contentContainerStyle={{flex: 1,  flexDirection: 'column', justifyContent: 'space-between'}}>
+            <ScrollView>
+                <DrawerItem key={"All List"}
+                            label={"All List"}
+                            activeBackgroundColor={darkTheme ? THEME_COLORS.DARK_THEME.PRIMARY : THEME_COLORS.LIGHT_THEME.PRIMARY}
+                            activeTintColor={darkTheme ? THEME_COLORS.DARK_THEME.ON_PRIMARY : THEME_COLORS.LIGHT_THEME.ON_PRIMARY}
+                            focused={activeList === 0}
+                            onPress={() => {
+                                setActiveList(0);
+                                props.navigation.navigate("All List");
+                            }}
+                            active={true}
                 />
-            })}
+                {databaseLists.map((list, listIndex) => {
+                    return <DrawerItem key={list.title}
+                                       label={list.title}
+                                       activeBackgroundColor={darkTheme ? THEME_COLORS.DARK_THEME.PRIMARY : THEME_COLORS.LIGHT_THEME.PRIMARY}
+                                       activeTintColor={darkTheme ? THEME_COLORS.DARK_THEME.ON_PRIMARY : THEME_COLORS.LIGHT_THEME.ON_PRIMARY}
+                                       focused={list.id === activeList}
+                                       onPress={() => {
+                                           setActiveList(list.id);
+                                           props.navigation.navigate(list.title);
+                                       }}
+                                       active={true}
+                    />
+                })}
+            </ScrollView>
+
+            <TextInput
+                style={{borderColor: darkTheme ? THEME_COLORS.DARK_THEME.PRIMARY : THEME_COLORS.LIGHT_THEME.PRIMARY,}}
+                label="New List"
+                onSubmit={() => {console.log("submit")}}
+                onChangeText={() => {}}
+            />
+
         </DrawerContentScrollView>
+
     )
 }
